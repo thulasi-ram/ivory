041c897b6906b1d77b61979c28d34793d3f8dabe
Thulasi Ram <thulasi503@gmail.com>
2018-05-01 14:41:44 +0530

    Corected profile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -15,11 +15,11 @@ class Salutation(TimeStampedModel):
 
 class Profile(TimeStampedModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    birth_day = models.DateField(default='', blank=True)
+    birthday = models.DateField(null=True, blank=True)
     linkedin_url = models.URLField(default='', blank=True)
     facebook_url = models.URLField(default='', blank=True)
     phone = models.CharField(max_length=15, default='', blank=True)
     alternate_phone = models.CharField(max_length=15, default='', blank=True)
     alternate_email = models.EmailField(max_length=200, default='', blank=True)
     gender = models.CharField(max_length=15, default='', blank=True)
-    salutation = models.ForeignKey(Salutation, null=True, on_delete=models.SET_NULL)
+    salutation = models.ForeignKey(Salutation, null=True, on_delete=models.SET_NULL, blank=True)
