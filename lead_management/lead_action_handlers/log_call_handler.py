8c333cb2c7a2fee3fe6bffc271efcea59d4f49a8
Thulasi Ram <thulasi503@gmail.com>
2018-05-02 01:34:54 +0530

    Missed lead update logic in action handlers

--- lead_management/lead_action_handlers/log_call_handler.py
+++ lead_management/lead_action_handlers/log_call_handler.py
@@ -1,4 +1,5 @@
 from actstream import action
+from django.db import transaction
 
 from lead_management.lead_action_handlers.base_handler import BaseActionHandler
 from lead_management.models import Lead
@@ -8,4 +9,8 @@ class LogCallActionHandler(BaseActionHandler):
 
     def handle(self, lead_id, user, **kwargs):
         lead = Lead.objects.get(uid=lead_id)
-        action.send(user, verb='called', action_object=lead)
+        with transaction.atomic():
+            if not lead.has_been_called:
+                lead.has_been_called = True
+                lead.save()
+            action.send(user, verb='called', action_object=lead)
--- lead_management/lead_action_handlers/send_profile_handler.py
+++ lead_management/lead_action_handlers/send_profile_handler.py
@@ -1,4 +1,5 @@
 from actstream import action
+from django.db import transaction
 
 from lead_management.lead_action_handlers.base_handler import BaseActionHandler
 from lead_management.models import Lead
@@ -8,4 +9,9 @@ class SendProfileActionHandler(BaseActionHandler):
 
     def handle(self, lead_id, user, **kwargs):
         lead = Lead.objects.get(uid=lead_id)
-        action.send(user, verb='sent profile to', target=lead)
+
+        with transaction.atomic():
+            if not lead.has_been_sent_profile:
+                lead.has_been_sent_profile = True
+                lead.save()
+            action.send(user, verb='sent profile to', target=lead)
