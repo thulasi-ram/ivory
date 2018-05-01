85b973003b887e9a15041b784abb56a895fa92fd
Thulasi Ram <thulasi503@gmail.com>
2018-05-01 23:54:19 +0530

    Fixed lead urls and images in feed

--- client_accounts/urls.py
+++ client_accounts/urls.py
@@ -10,10 +10,10 @@ from client_accounts.views.legal_entity import LegalEntityView
 
 urlpatterns = [
     url(r'^$', ClientAccountLanding.as_view(), name='landing'),
-    url(r'^client/(?P<client_id>.*)', ClientView.as_view(), name='client'),
-    url(r'^clients/', ClientsView.as_view(), name='clients'),
-    url(r'^legal_entity/(?P<legal_entity_id>.*)', LegalEntityView.as_view(), name='legal_entity'),
-    url(r'^legal_entities/', LegalEntitiesView.as_view(), name='legal_entities'),
+    url(r'^client/(?P<client_id>.*)/?', ClientView.as_view(), name='client'),
+    url(r'^clients/?', ClientsView.as_view(), name='clients'),
+    url(r'^legal_entity/(?P<legal_entity_id>.*)/?', LegalEntityView.as_view(), name='legal_entity'),
+    url(r'^legal_entities/?', LegalEntitiesView.as_view(), name='legal_entities'),
 
     # autocompletes
     url(r'^client-account-autocomplete', ClientAccountAutocomplete.as_view(), name='client-account-autocomplete'),
